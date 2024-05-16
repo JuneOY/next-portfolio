3026593b10fa73730817b229c729de99c7c6c2bc
Xou <1192158284@qq.com>
2024-05-16 09:17:31 +0800

    feat: init

---
+++ app/about-me/page.tsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <main id="about-me" className="page">
+      <div id="mobile-page-title">
+        <h2>_about-me</h2>
+      </div>
+    </main>
+  );
+}
+
+export default About;
